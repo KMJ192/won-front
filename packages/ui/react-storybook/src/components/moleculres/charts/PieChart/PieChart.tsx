056199297d06514cf7d9fa3edf8659dd4f518cf8
KMJ192 <56943526+KMJ192@users.noreply.github.com>
2021-10-30 00:15:49 +0900

    [storybook][기능] pie chart 컴포넌트 수정

--- packages/ui/react-storybook/src/components/moleculres/charts/PieChart/PieChart.tsx
+++ packages/ui/react-storybook/src/components/moleculres/charts/PieChart/PieChart.tsx
@@ -18,16 +18,19 @@ PieChart.defaultProps = {
   customStyle: undefined,
 };
 
+const angle = Math.PI / 180;
+
 function PieChart({ data, size, customStyle }: Props) {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const [canvas, setCanvas] = useState<HTMLCanvasElement | null>(null);
   const [ctx, setCtx] = useState<CanvasRenderingContext2D | null>(null);
+  const [eArr, setEArr] = useState<PieChartData[][]>([]);
+  const [radius, setRadius] = useState<number>(0);
 
   // pie chart draw
   const drawArc = useCallback(
     (convArray: number[], circleSize: Position) => {
       if (ctx && circleSize.width && circleSize.height) {
-        const radius = Math.floor(circleSize.width / 2) - 5;
-        const angle = Math.PI / 180;
         let degree = 0;
 
         const eventArray: PieChartData[][] = data.map(() => []);
@@ -73,9 +76,10 @@ function PieChart({ data, size, customStyle }: Props) {
           ctx.stroke();
           ctx.restore();
         }
+        setEArr(eventArray);
       }
     },
-    [ctx, data],
+    [ctx, data, radius],
   );
 
   // Pie Chart를 그릴 각도 계산 및 Chart 그리는 함수 실행
@@ -99,65 +103,134 @@ function PieChart({ data, size, customStyle }: Props) {
     [drawArc],
   );
 
-  const isInsideArc = (x: number, y: number, canvas: HTMLCanvasElement): PieChartEvent => {
-    const circleSize = {
-      width: canvas.clientWidth,
-      height: canvas.clientHeight,
-    };
+  const isInsideArc = useCallback(
+    (x: number, y: number, canvas: HTMLCanvasElement): PieChartEvent => {
+      const posX = canvas.clientWidth / 2 - x;
+      const posY = canvas.clientHeight / 2 - y;
+      const circleLen = Math.floor(canvas.clientWidth / 2) - 5;
+      const len = Math.sqrt(Math.abs(posX * posX) + Math.abs(posY * posY));
 
-    const circleLen = Math.floor(circleSize.width / 2) - 5;
+      let rad = Math.atan2(posY, posX);
+      rad = (rad * 180) / Math.PI;
 
-    const posX = circleSize.width / 2 - x;
-    const posY = circleSize.height / 2 - y;
-    const len = Math.sqrt(Math.abs(posX * posX) + Math.abs(posY * posY));
-
-    let rad = Math.atan2(posY, posX);
-    rad = (rad * 180) / Math.PI;
-
-    let retVal: PieChartEvent = {
-      result1: false,
-      result2: false,
-      index: -1,
-      degree: rad,
-    };
-
-    if (len <= circleLen) {
-      retVal = {
-        ...retVal,
-        result1: true,
+      let retVal: PieChartEvent = {
+        result1: false,
+        result2: false,
+        index: -1,
+        degree: rad,
       };
-    }
 
-    return retVal;
-  };
+      if (len <= circleLen) {
+        retVal = {
+          ...retVal,
+          result1: true,
+        };
+      }
 
-  const canvasClickEvent = useCallback((e: MouseEvent) => {
-    const canvas: HTMLCanvasElement | null = canvasRef.current;
-    if (canvas) {
-      const x: number = e.clientX - canvas.offsetLeft;
-      const y: number = e.clientY - canvas.offsetTop;
+      if (retVal.result1) {
+        eArr.forEach((arr: PieChartData[], idx: number) => {
+          if (rad >= arr[0].value && rad <= arr[1].value) {
+            retVal.result2 = true;
+            retVal.index = idx;
+          }
+        });
+      }
 
-      const inn: PieChartEvent = isInsideArc(x, y, canvas);
-      console.log(inn);
-    }
-  }, []);
+      return retVal;
+    },
+    [eArr],
+  );
 
+  const hoverCanvas = useCallback(
+    (idx: number) => {
+      if (ctx && canvas) {
+        let degree: number = 360;
+
+        ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+
+        const sum: PieChartData = data.reduce((a, b) => {
+          return { value: a.value + b.value };
+        });
+        const convArray = data.map((val: PieChartData) => {
+          const rate = val.value / sum.value;
+          const myDegree = degree * rate;
+          return myDegree;
+        });
+
+        for (let i: number = 0; i < convArray.length; i++) {
+          const item: number = convArray[i];
+
+          ctx.save();
+          ctx.beginPath();
+
+          let innRadius = radius;
+          ctx.moveTo(canvas.clientWidth / 2, canvas.clientHeight / 2);
+          if (idx === i) {
+            ctx.lineWidth = 2;
+            ctx.strokeStyle = 'blue';
+            innRadius = radius * 1.1;
+          }
+          if (idx === 0) {
+            ctx.arc(canvas.clientWidth / 2, canvas.clientHeight / 2, innRadius, 0, angle * item, false);
+            degree = item;
+          } else {
+            ctx.arc(
+              canvas.clientWidth / 2,
+              canvas.clientHeight / 2,
+              innRadius,
+              angle * degree,
+              angle * (degree + item),
+              false,
+            );
+            degree += item;
+          }
+          ctx.closePath();
+          ctx.stroke();
+          ctx.restore();
+        }
+      }
+    },
+    [canvas, ctx, data, radius],
+  );
+
+  const canvasMouseEvent = useCallback(
+    (e: MouseEvent) => {
+      let drawed: boolean = false;
+      if (canvas) {
+        const x: number = e.clientX - canvas.offsetLeft;
+        const y: number = e.clientY - canvas.offsetTop;
+        const inn: PieChartEvent = isInsideArc(x, y, canvas);
+
+        if (inn.index > -1) {
+          drawed = true;
+          hoverCanvas(inn.index);
+        } else {
+          if (drawed) hoverCanvas(-1);
+          drawed = false;
+        }
+      }
+    },
+    [canvas, hoverCanvas, isInsideArc],
+  );
+
+  // draw chart
   useEffect(() => {
-    if (canvasRef.current) {
+    if (canvasRef.current && data) {
+      const rad = Math.floor(canvasRef.current.clientWidth / 2) - 5;
+      setCanvas(canvasRef.current);
       setCtx(canvasRef.current.getContext('2d'));
+      calcData(canvasRef.current, data);
+      setRadius(rad);
     }
-  }, []);
+  }, [calcData, data]);
 
+  // mouse 이벤트 등록
   useEffect(() => {
-    const canvas: HTMLCanvasElement | null = canvasRef.current;
-    canvas?.addEventListener('click', canvasClickEvent);
-    if (canvas && data) {
-      calcData(canvas, data);
-    }
+    canvas?.addEventListener('mousemove', canvasMouseEvent);
     return () => {
-      canvas?.removeEventListener('click', canvasClickEvent);
+      canvas?.removeEventListener('mousemove', canvasMouseEvent);
     };
-  }, [drawArc, data, calcData, canvasClickEvent]);
+  }, [canvas, canvasMouseEvent]);
 
   return (
     <canvas
